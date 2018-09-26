978379f1dfd4b03803fa4871f500fd07b40bce49
Stephen Ruiz <steveruizok@gmail.com>
2018-09-26 21:37:10 +0100

    Basics
    The idea here is to have a config file that holds different user configurations. A configuration can include npm packages to install and boilerplate for new components. It might make more sense to separate these out, however they may often be connected (e.g. when boilerplate imports packages).

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
 const chalk = require("chalk");
+const pkg = require("./package.json");
 const clear = require("clear");
 const files = require("./lib/files");
 const prompt = require("./lib/prompt");
 const create = require("./lib/create");
+const { conf } = require("./lib/config");
 
-clear();
+const msg = (string, color = "yellow") => {
+	console.log(chalk[color](string));
+};
+
+// Do intro stuff
 
 const runIntroduction = async () => {
 	clear();
@@ -14,43 +20,65 @@ const runIntroduction = async () => {
 		chalk.blueBright.bold(`
 Framer Tools`)
 	);
-	console.log(chalk.blue(`v0.1.4`));
+	msg(`v${pkg.version}`, "blue");
+};
 
+// Check to see if we're in a Framer X project folder
+
+const checkForFolder = async () => {
 	if (files.getCurrentDirectoryBase() !== "container") {
-		console.log(
-			chalk.yellow(`
+		msg(`
 ⚠️  Hey, it looks like you're not in a Framer X project folder. 
-		`)
-		);
+		`);
 
 		const { willContinue } = await prompt.continue();
 		if (!willContinue) {
-			console.log(
-				chalk.green(`
+			msg(
+				`
 Ok, exiting process. 👋
 
 Try running this command again after opening your Framer X 
 project's project folder (File > Show Proejct Folder). 
-		`)
+		`,
+				"green"
 			);
 			process.exit();
 		}
 	}
+
+	return;
 };
 
-const createNewComponent = async () => {
-	console.log(`
-> Create new component`);
+// Create a component
 
+const createNewComponent = async () => {
 	const { componentName } = await prompt.askComponentName();
 	await create.createComponent(componentName);
-	console.log(
-		chalk.green(`
+	msg(
+		`
 📦  Component created at ./code/${componentName}.tsx!
-`)
+`,
+		"green"
 	);
 	return componentName;
 };
+
+// Get the first action
+const getFirstAction = async () => {
+	const { firstAction } = await prompt.getFirstAction();
+
+	if (firstAction === "Create a component") {
+		let componentName = await createNewComponent();
+		getNextAction(componentName);
+	} else if (firstAction === "Set default configuration") {
+		await setDefaultConfiguration();
+		getFirstAction();
+	} else {
+		exitProcess();
+	}
+};
+
+// Get the next action
 
 const getNextAction = async lastComponentName => {
 	const { nextAction } = await prompt.followUp(lastComponentName);
@@ -58,28 +86,101 @@ const getNextAction = async lastComponentName => {
 	if (nextAction === `Open component (${lastComponentName})`) {
 		await files.openComponent(lastComponentName);
 		getNextAction(lastComponentName);
-		return;
-	}
-
-	if (nextAction === `Create another`) {
+	} else if (nextAction === `Create another`) {
 		let componentName = await createNewComponent();
 		getNextAction(componentName);
-		return;
+	} else {
+		exitProcess();
+	}
+};
+
+// Setup configuration for first time users
+
+const setupConfig = async () => {
+	conf.delete("saved");
+
+	if (Object.keys(conf.all).length === 0) {
+		msg("No config found, creating config... ");
+	} else {
+		msg("Loading config... ");
 	}
 
-	console.log(
-		chalk.yellow(`
+	if (!conf.get("saved")) {
+		msg("Creating defaults... ");
+		conf.set("saved", {});
+		conf.set("saved", [
+			{
+				name: "Default",
+				installs: "",
+				imports: ""
+			},
+			{
+				name: "Emotion",
+				installs: "npm install emotion",
+				imports: `import { css } from "emotion"`
+			}
+		]);
+		conf.set("selectedConfig", "Default");
+		conf.set("defaultConfig", "Default");
+	}
+
+	return;
+};
+
+// Exit
+
+const exitProcess = () => {
+	msg(`
 👋  Ciao! 
-`)
-	);
+`);
 	process.exit();
 };
 
-const run = async () => {
-	await runIntroduction();
+// Choose a configuration
 
-	let lastComponentName = await createNewComponent();
-	getNextAction(lastComponentName);
+const selectConfig = async () => {
+	const { configToUse } = await prompt.askForConfig();
+	if (configToUse === "Create new") {
+		msg("Creating a new action...");
+		return;
+	}
+
+	conf.set("selectedConfig", configToUse);
+	return;
 };
 
+const setDefaultConfig = async () => {
+	msg(`
+Your current default configuration is ${conf.get("defaultConfig")}.
+`);
+	const { newDefault } = await prompt.changeDefaultConfig();
+
+	if (newDefault === "Go back") {
+		msg(`
+Keeping ${conf.get("defaultConfig")} as default config.
+		`);
+		return;
+	}
+
+	conf.set("defaultConfig", newDefault);
+	msg(`
+Setting ${conf.get("defaultConfig")} as new default config.
+		`);
+};
+
+const run = async () => {
+	await setupConfig();
+	await setDefaultConfig();
+	await getFirstAction();
+	// await runIntroduction();
+	// await checkForFolder();
+
+	// await selectConfig();
+	// let lastComponentName = await createNewComponent();
+	// getNextAction(lastComponentName);
+	msg("Ending");
+	process.exit();
+};
+
+clear();
 run();
